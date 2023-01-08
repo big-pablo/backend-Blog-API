00d12f467e506fe82deada3b33fed53dd182f675
Pavlov Leonid <pavloff.leonid@gmail.com>
2023-01-08 20:16:26 +0700

    Task #16559 & Task #16562 #16559: Возможно придётся исправить, потому что некоторая логика и обработка ошибок выполняется в контроллере, что вроде как не есть хорошо #16562: Скопировал сервис из прошлого проекта, если написанное выше надо будет исправлять, контроллер надо будет применять в других контроллерах или вообще убрать

---
+++ Blog.API/Controllers/UsersController.cs
@@ -0,0 +1,83 @@
+﻿using Blog.API.Models.DTOs;
+using Blog.API.Services;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Blog.API.Controllers
+{
+    [Route("api/account")]
+    [ApiController]
+    public class UsersController : ControllerBase
+    {
+        private IAuthService _authService;
+        private IUserService _userService;
+        private IInnerService _innerService;
+        public UsersController(IAuthService authInterface, IUserService userService, IInnerService innerService)
+        {
+            _authService = authInterface;
+            _userService = userService;
+            _innerService = innerService;
+        }
+        [HttpPost("register")]
+        public async Task<ActionResult<TokenResponseDTO>> Register(UserRegisterDTO model)
+        {
+            try
+            {
+                return await _authService.Register(model);
+            }
+            catch
+            {
+                return BadRequest("This mail is already in use");
+            }
+        }
+        [HttpPost("login")]
+
+        public async Task<ActionResult<TokenResponseDTO>> Login(LoginCredentialsDTO model)
+        {
+            try
+            {
+                return await _authService.Login(model);
+            }
+            catch
+            {
+                return BadRequest("Invalid username or password");
+            }
+        }
+
+        [HttpPost("logout")]
+        [Authorize]
+        public async Task<IActionResult> Logout()
+        {
+            if (await _innerService.TokenIsInBlackList(HttpContext.Request.Headers)) return Unauthorized("The user is not authorized");
+            var token = await _innerService.GetToken(HttpContext.Request.Headers);
+            try
+            {
+                await _authService.Logout(token);
+                return Ok();
+            }
+            catch
+            {
+                return BadRequest("The user is not authorized");
+            }
+        }
+
+        [HttpGet("profile")]
+        [Authorize]
+        public async Task<ActionResult<UserDTO>> GetUserData()
+        {
+            if (await _innerService.TokenIsInBlackList(HttpContext.Request.Headers)) return Unauthorized("The user is not authorized");
+            var response = await _userService.GetUserProfile(await _innerService.GetUserId(HttpContext.User));
+            return Ok(response);
+        }
+        [HttpPut("profile")]
+        [Authorize]
+        public async Task<IActionResult> UpdateUserData(UserEditDTO model)
+        {
+            if (await _innerService.TokenIsInBlackList(HttpContext.Request.Headers)) return Unauthorized("The user is not authorized");
+            await _userService.UpdateUserProfile(await _innerService.GetUserId(HttpContext.User), model);
+            return Ok();
+        }
+    }
+
+
+}
---
+++ Blog.API/Services/InnerService.cs
@@ -0,0 +1,40 @@
+﻿using Blog.API.Models;
+using Blog.API.Models.DTOs;
+using Microsoft.AspNetCore.Mvc;
+using System.Security.Claims;
+
+namespace Blog.API.Services
+{
+    public interface IInnerService
+    {
+        public Task<string> GetToken(IHeaderDictionary headerDictionary);
+        public Task<string> GetUserId(ClaimsPrincipal principal);
+        public Task<bool> TokenIsInBlackList(IHeaderDictionary headerDictionary);
+    }
+    public class InnerService
+    {
+        private readonly Context _context;
+        public InnerService(Context context)
+        {
+            _context = context;
+        }
+
+        public async Task<string> GetToken(IHeaderDictionary headerDictionary)
+        {
+            var autorizationSrting = Convert.ToString(headerDictionary.Authorization);
+            var token = autorizationSrting.Replace("Bearer ", "");
+            return token;
+        }
+        public async Task<string> GetUserId(ClaimsPrincipal principal)
+        {
+            return principal.Claims.SingleOrDefault(p => p.Type == ClaimTypes.NameIdentifier).Value;
+        }
+        public async Task<bool> TokenIsInBlackList(IHeaderDictionary headerDictionary)
+        {
+            var token = await GetToken(headerDictionary);
+            var tokenEntity = _context.TokenEntities.FirstOrDefault(x => x.Token == token);
+            return !(tokenEntity == null);
+        }
+
+    }
+}
