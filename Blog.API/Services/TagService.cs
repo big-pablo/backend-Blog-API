7b9c5f17ad721130d5e0185d18e837c7abd1ab1d
Pavlov Leonid <pavloff.leonid@gmail.com>
2023-01-08 20:42:35 +0700

    Task #16563 Написал, но мб придётся переделать под возвращение List<TagDTO> (Хотя они равноценны)

---
+++ Blog.API/Services/TagService.cs
@@ -0,0 +1,25 @@
+﻿using Blog.API.Models;
+using Blog.API.Models.DTOs;
+using Blog.API.Models.Entities;
+
+namespace Blog.API.Services
+{
+    public interface ITagService
+    {
+        public Task<List<TagEntity>> GetTags();
+    }
+
+    public class TagService:ITagService
+    {
+        private readonly Context _context;
+        public TagService(Context context)
+        {
+            _context = context;
+        }
+        public async Task<List<TagEntity>> GetTags()
+        {
+            List<TagEntity> tagEntityList = _context.TagEntities.ToList(); //Мб надо будет переделать, чтобы оно возвращало List<TagDTO>
+            return tagEntityList;
+        }
+    }
+}
