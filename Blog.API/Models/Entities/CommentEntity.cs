2beb36170c5d2042986cf031283cd1bda831c197
Pavlov Leonid <pavloff.leonid@gmail.com>
2023-01-12 02:32:13 +0700

    Task #16690 Готово, правки буду вносить потом. Подписал комменты, что надо исправить, внутренний сервис не подключал, он в другом коммите. Все переделывания буду делать после слияния всех веток

--- Blog.API/Models/Entities/CommentEntity.cs
+++ Blog.API/Models/Entities/CommentEntity.cs
@@ -4,8 +4,10 @@
     {
         public string Id { get; set; }
         public PostEntity Post { get; set; }
+        public string Content { get; set; }
         public UserEntity User { get; set; }
         public CommentEntity? ParentComment { get; set; }
-
+        public DateTime? ModifiedDate { get; set; }
+        public DateTime? DeleteDate { get; set; }
     }
 }
